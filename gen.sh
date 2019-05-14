c54da0076c12284e475316e76d17148ad93b89c4
Allen-Cee <cee.quant@gmail.com>
2019-05-15 03:30:55 +0800

    update

--- gen.sh
+++ gen.sh
@@ -3,14 +3,14 @@ function GenerateIndexFile()
 	file_path=$(pwd)
 	echo $file_path
 
-	files=$(ls)
+	files=$(ls -F)
 	main=index.html
 	cat /dev/null > $main
 	echo '<html>\n<head>\n<title>Index of</title>\n<body bgcolor="white">\n<h1>Index of /'${file_path:46}'</h1>\n<hr>\n<pre>' > $main
 	echo '<a href="../">../</a>' >> $main
 	for f in $files; do
 		if [ $f != 'index.html' ] && [ $f != 'gen.sh' ] && [ $f != 'README.md' ]; then
-			echo '<a href="'$f'/">'$f'/</a>' >> $main
+			echo '<a href="'$f'">'$f'/</a>' >> $main
 		fi
 	done
 	echo '</pre>\n<hr>\n</body>\n</html>' >> $main
