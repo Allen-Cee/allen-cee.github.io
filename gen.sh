f147f6907ebf007dc0aca3a217172945f9fe34c2
cee.quant@gmail.com <cee.quant@gmail.com>
2019-09-24 16:44:19 +0800

    update

--- gen.sh
+++ gen.sh
@@ -1,39 +1,41 @@
 #!/bin/bash
 
+python3 /Users/Aoi/Desktop/Quant/fund/黑流/流形一期/convert.py
+/Users/Aoi/Documents/Code/allen-cee.github.io/zip.sh
+
 function GenerateIndexFile()
 {
-	file_path=$(pwd)
-	echo $file_path
+    file_path=$(pwd)
+    echo $file_path
+    files=$(ls -F)
+    main=index.html
+    cat /dev/null > $main
+    printf "<html>\n<head>\n<title>Index of "${file_path:45}"/</title>\n<body bgcolor=\"white\">\n<h1>Index of "${file_path:45}"/</h1>\n<hr>\n<pre>\n" > $main
+    printf "<a href=\"../\">../</a>\n" >> $main
+    for f in $files; do
+        if [ $f != 'index.html' ] && [ $f != 'gen.sh' ] && [ $f != 'zip.sh' ] && [ $f != 'README.md' ]; then
+            f=${f/' '/'_'}
+            time=$(date -r $f '+%d-%b-%Y %H:%M')
+            size='-'
+            if [ -f $f ]; then
+                size=$(stat -f %z $f)
+                if [ ${#f} -gt 50 ]; then
+                    f=${f:0:47}'..>'
+                fi
+            fi
+            printf '%-'$[65+${#f}]'s %s %20s\n' '<a href="'$f'">'$f'</a>' "$time" "$size" >> $main
+        fi
+    done
+    printf "</pre>\n</hr>\n</body>\n</html>\n" >> $main
 
-	files=$(ls -F)
-	main=index.html
-	cat /dev/null > $main
-	printf "<html>\n<head>\n<title>Index of "${file_path:45}"/</title>\n<body bgcolor=\"white\">\n<h1>Index of "${file_path:45}"/</h1>\n<hr>\n<pre>\n" > $main
-	printf "<a href=\"../\">../</a>\n" >> $main
-	for f in $files; do
-		if [ $f != 'index.html' ] && [ $f != 'gen.sh' ] && [ $f != 'README.md' ]; then
-			f=${f/' '/'_'}
-			time=$(date -r $f '+%d-%b-%Y %H:%M')
-			size='-'
-			if [ -f $f ]; then
-				size=$(stat -f %z $f)
-				if [ ${#f} -gt 50 ]; then
-					f=${f:0:47}'..>'
-				fi
-			fi
-			printf '%-'$[65+${#f}]'s %s %20s\n' '<a href="'$f'">'$f'</a>' "$time" "$size" >> $main
-		fi
-	done
-	printf "</pre>\n</hr>\n</body>\n</html>\n" >> $main
-
-	for f in $files; do
-		if [ -d $f ]; then
-			cd $f;
-			#file_path=$(pwd)
-			GenerateIndexFile;
-			cd ..;
-		fi
-	done
+    for f in $files; do
+        if [ -d $f ]; then
+           cd $f;
+           #file_path=$(pwd)
+           GenerateIndexFile;
+           cd ..;
+        fi
+    done
 }
 
 GenerateIndexFile;
---
+++ zip.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/expect
+
+spawn zip -e /Users/Aoi/Documents/Code/allen-cee.github.io/网络/shadowsocks/shadowsocks_profiles.zip /Users/Aoi/Desktop/shadowsocks_profiles.txt
+expect "Enter password:"
+send "cyys1909\r"
+expect "Verify password:"
+send "cyys1909\r"
+interact
+
+spawn zip -e /Users/Aoi/Documents/Code/allen-cee.github.io/黑流投资/流形一期/流形一期公告净值.zip /Users/Aoi/Desktop/Quant/fund/黑流/流形一期/流形一期公告净值.csv
+expect "Enter password:"
+send "tlms0515\r"
+expect "Verify password:"
+send "tlms0515\r"
+interact
